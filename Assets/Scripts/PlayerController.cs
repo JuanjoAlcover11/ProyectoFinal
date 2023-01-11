6c4ab0361f8f4881b2a162338e4a92ca7c27ec34
JuanjoAlcover11 <61268085+JuanjoAlcover11@users.noreply.github.com>
2023-01-11 12:19:18 +0100

    11/01

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,10 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     public float moveSpeed;
     public float jumpForce;
+    public float rotateSpeed;
     public float gravityScale = 5f;
 
     public CharacterController charController;
     public Camera playerCamera;
+    public GameObject playerModel;
+
+    public Animator animator;
 
     private Vector3 moveDirection;
 
@@ -22,15 +26,20 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float yStore = moveDirection.y;
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        //moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
+        moveDirection.Normalize();
         moveDirection = moveDirection * moveSpeed;
-        moveDirection.y = yStore;
+        moveDirection.y = yStore ;
 
-        if (Input.GetKeyDown("space"))
+        if (charController.isGrounded)
         {
-            moveDirection.y = jumpForce;
-        }
 
+            if (Input.GetKeyDown("space"))
+            {
+                moveDirection.y = jumpForce;
+            }
+        }
         moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
 
         charController.Move(moveDirection * Time.deltaTime);
@@ -38,6 +47,11 @@ public class PlayerController : MonoBehaviour
         if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
         {
             transform.rotation = Quaternion.Euler(0f, playerCamera.transform.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
         }
+
+        animator.SetFloat("Speed", Mathf.Abs(moveDirection.x) + Mathf.Abs(moveDirection.z));
+        animator.SetBool("isGrounded", charController.isGrounded);
     }
 }
